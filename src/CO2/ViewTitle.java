5558ef9927421b880ac41070f3a36ec550c9cb34
Yassine SMARA <yassine.smara@laposte.net>
2020-11-05 10:49:33 +0100

    Desactivation du choix du nombre de joueurs

--- src/CO2/ViewTitle.java
+++ src/CO2/ViewTitle.java
@@ -19,7 +19,7 @@ public class ViewTitle {
     Pane paneGame;
 
     Button btn;
-    ComboBox<String> comboBox;
+    //ComboBox<String> comboBox;
     
     public ViewTitle(Model model) {
 
@@ -37,8 +37,11 @@ public class ViewTitle {
 		btn.setStyle("-fx-background-color: transparent; -fx-text-fill: #e46d31; -fx-border-color:  #E46D31;-fx-border-width: 2px; -fx-border-style: solid; -fx-border-radius: 30;-fx-font-size: 30px;-fx-fill-width: 150px;");
 		btn.setOnMouseEntered(e -> btn.setStyle("-fx-background-color: #E46D31;-fx-background-radius:30  ;-fx-text-fill: white; -fx-font-size: 30px;-fx-cursor: HAND"));
 		btn.setOnMouseExited(e -> btn.setStyle("-fx-background-color: transparent; -fx-text-fill: #E46D31; -fx-border-color:  #E46D31;-fx-border-width: 2px; -fx-border-style: solid; -fx-border-radius: 30;-fx-font-size: 30px;"));
+
 		btn.setLayoutX(970);
-		btn.setLayoutY(600);
+		// Valeur pour plusieur joueur
+		// btn.setLayoutY(600);
+		btn.setLayoutY(400);
 
 		Image imgMainTitle = new Image("CO2/images/CO2MainTitle.jpg");
 		ImageView imageViewMainTitle = new ImageView(imgMainTitle);
@@ -48,6 +51,7 @@ public class ViewTitle {
 		imageViewMainTitle.setFitHeight(model.height);
 
 
+		/* Le choix du joueur a été desactivé
 		comboBox = new ComboBox<>();
 		comboBox.getItems().addAll("1","2","3","4","5");
 		comboBox.getSelectionModel().selectFirst();
@@ -55,20 +59,24 @@ public class ViewTitle {
 		comboBox.setLayoutX(1050);
 		comboBox.setLayoutY(390);
 
+
 		Label lbNbJoueur = new Label("Choisir le nombre de joueur : ");
 		lbNbJoueur.setStyle("-fx-text-fill: #E46D31;-fx-font-size: 30;-fx-font-weight: bold");
 		lbNbJoueur.setLayoutX(900);
 		lbNbJoueur.setLayoutY(300);
+		 */
 
 		paneIntro.getChildren().add(imageViewMainTitle);
-		paneIntro.getChildren().add(lbNbJoueur);
-		paneIntro.getChildren().add(comboBox);
+		// Choix du joueur desactivé
+		//paneIntro.getChildren().add(lbNbJoueur);
+		//paneIntro.getChildren().add(comboBox);
 		paneIntro.getChildren().add(btn);
     }
 
 	public void setButtonControler(EventHandler<ActionEvent> handler) {
 		btn.setOnAction(handler);
-		comboBox.setOnAction(handler);
+		// Choix du joueur desactivé
+		//comboBox.setOnAction(handler);
 	}
 
     public void startGame() {
