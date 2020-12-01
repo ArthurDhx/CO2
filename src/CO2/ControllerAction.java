8eb449a7741de6ceffa19643031e5e38823fd4ea
Théo GANARD <theo.ganard@gmail.com>
2020-12-01 13:14:14 +0100

    Fix bug expertise

--- src/CO2/ControllerAction.java
+++ src/CO2/ControllerAction.java
@@ -71,8 +71,8 @@ public class ControllerAction implements EventHandler<ActionEvent>{
             // niveau d'expertise du joueur dans ce type d'expertise
             int playerExp = p.getExpertise(type);
             // nombre de revenu correspondant
-            int revenu = exp.getPiste().get(playerExp).getNumero();
-            //int revenu = exp.getPiste().get(playerExp-1).getNumero(); semble corriger le problème
+            int revenu = 0;
+            if (playerExp > 0) revenu = exp.getPiste().get(playerExp-1).getNumero();
 
             // si il y a un revenu a distribuer
             if (revenu>0) {
