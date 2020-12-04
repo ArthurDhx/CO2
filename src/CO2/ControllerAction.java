35ecc5efeaca49a5695cb16d648f921476767dd9
Othen <duboisevan02@gmail.com>
2020-12-04 14:58:34 +0100

    assez de centrales.. ou pas !
    savoir pour chaque continent si il a assez de central ou pas

--- src/CO2/ControllerAction.java
+++ src/CO2/ControllerAction.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Optional;
 
 public class ControllerAction implements EventHandler<ActionEvent>{
@@ -26,38 +27,7 @@ public class ControllerAction implements EventHandler<ActionEvent>{
         } else if (source == viewGame.hboxAction.btnCancelAction) {
             viewGame.hboxAction.resetHbox();
         }else if (source == viewGame.hboxAction.btnFinTour) { // appuyer sur bouton fin de tour
-            // si le scientifique n'est pas retourné dans la réserve
-            // l'expertise lors du retour d'un scientifique à la réserve est géré par le ControllerActionGratuite
-            if (model.getCurrentPLayer().getCurrentScientifique().getContinent() != null){
-                choisirScientifiqueExpertise();
-            }
-            // vérification du nombre de tour et décénnie
-            model.TourSuivant();
-            // si on est au tour 1 de n'importe quelle decenie sauf la première
-            if ((model.getTour() == 1) && (model.getDecade() != 1970)) {
-                // ~= phase d'approvisionnement
-                distributionRevenu();
-            }
-            model.endGame();
-            // récompense sommets
-            model.giveRewardsSommet();
-            model.getCurrentPLayer().setActionPrincipaleDone(false);
-            model.getCurrentPLayer().setDeplacerScientifiqueDone(false);
-            model.getCurrentPLayer().setMarcheCEPDone(false);
-            viewGame.hboxAction.resetHbox();
-            // actualisation du nombre de tour et de décénnie
-            viewGame.reloadTour();
-            viewGame.reloadDecade();
-            // affichage sur la console le nombre de tour et décénnie
-            System.out.println("Tour : " + model.getTour() + "/" + (model.NB_TOUR_PAR_DECENNIE-1));
-            System.out.println("Décénnie : " + model.getDecade() + "/" + model.NB_DECENNIE);
-            // model.tilesSolarProjects.get(0).setSubventionPossible(false); // temporaire
-            // affiche message si fin de partie
-            try {
-                viewGame.isEndGame();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+            finTour();
         }
         viewGame.reloadArgent();
     }
@@ -102,5 +72,103 @@ public class ControllerAction implements EventHandler<ActionEvent>{
                 return;
             });
         }
+    }
+
+    public void finTour(){
+        // si le scientifique n'est pas retourné dans la réserve
+        // l'expertise lors du retour d'un scientifique à la réserve est géré par le ControllerActionGratuite
+        if (model.getCurrentPLayer().getCurrentScientifique().getContinent() != null){
+            choisirScientifiqueExpertise();
+        }
+        // vérification du nombre de tour et décénnie
+        model.TourSuivant();
+        // si on est au tour 1 de n'importe quelle decenie sauf la première
+        if ((model.getTour() == 1) && (model.getDecade() != 1970)) {
+            // ~= phase d'approvisionnement
+            distributionRevenu();
+            //approvisionnement en énergie
+            ArrayList<Continent> continentsEnBesoin = new ArrayList<>();
+            if(model.getDecade() == 1970){
+                continentsEnBesoin = verifierNbCentral(1);
+            }
+            else if(model.getDecade() == 1980){
+                continentsEnBesoin = verifierNbCentral(2);
+            }
+            else if(model.getDecade() == 1990){
+                continentsEnBesoin = verifierNbCentral(3);
+            }
+            else if(model.getDecade() == 2000){
+                continentsEnBesoin = verifierNbCentral(4);
+            }
+            else if(model.getDecade() == 2010){
+                continentsEnBesoin = verifierNbCentral(5);
+            }
+            //Seulement pour le multi :
+            else if(model.getDecade() == 2020){
+                continentsEnBesoin = verifierNbCentral(6);
+            }
+            if(continentsEnBesoin.isEmpty()){ //Si aucun continent n'est en besoin de centrale
+                //affichage d'un message
+                viewGame.displayAlertWithoutHeaderText("Bien joué !", "Vous avez bien joué durant cette décennie. Aucun contient ne manque d'énergie !");
+            }
+            else{
+                viewGame.displayAlertWithoutHeaderText("Problème !", "Il manque des centrales !");
+            }
+        }
+        model.endGame();
+        // récompense sommets
+        model.giveRewardsSommet();
+        model.getCurrentPLayer().setActionPrincipaleDone(false);
+        model.getCurrentPLayer().setDeplacerScientifiqueDone(false);
+        model.getCurrentPLayer().setMarcheCEPDone(false);
+        viewGame.hboxAction.resetHbox();
+        // actualisation du nombre de tour et de décénnie
+        viewGame.reloadTour();
+        viewGame.reloadDecade();
+        // affichage sur la console le nombre de tour et décénnie
+        System.out.println("Tour : " + model.getTour() + "/" + (model.NB_TOUR_PAR_DECENNIE-1));
+        System.out.println("Décénnie : " + model.getDecade() + "/" + model.NB_DECENNIE);
+        // model.tilesSolarProjects.get(0).setSubventionPossible(false); // temporaire
+
+        // affiche message si fin de partie
+        try {
+            viewGame.isEndGame();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * permet de savoir si tout les contients on le bon nombre de central
+     * @param nbCentral
+     * @return la liste des continents dans le besoin
+     */
+    public ArrayList<Continent> verifierNbCentral(int nbCentral){
+        Continent[] continents = model.getContinents();
+        ArrayList<Continent> continentsEnBesoin = new ArrayList<>();
+        for(int i = 0; i < continents.length; i++){
+            //Récupération des case central du continent
+            ArrayList<Central> caseCentrals = continents[i].getCentrales();
+            //Variables pour stocker le nombre de central dans le continent
+            int nbCentralInContinent = 0;
+            //On regarde le nombre de case qui sont occupée ou non
+            for (Central c: caseCentrals){
+                //Si la case central est occupée, on ajout 1 au nombre de central dans le continent
+                if(c.isOccupe()) nbCentralInContinent += 1;
+            }
+            //On regarde si le nombre de central dans le continent est suffisant ou non
+            if(nbCentralInContinent < nbCentral && nbCentralInContinent < continents[i].getCentrales().size()){
+                String title = "ATTENTION ! L'"+continents[i].getName()+" a besoin de central";
+                String message = "Il manque "+(nbCentral-nbCentralInContinent)+" centrale(s) en "+continents[i].getName();
+                viewGame.displayAlertWithoutHeaderText(title, message);
+            }
+            else{
+                String title = "Tout va bien !";
+                String message = "Il ne manque aucunes centrales en "+continents[i].getName();
+                viewGame.displayAlertWithoutHeaderText(title, message);
+                continentsEnBesoin.add(continents[i]);
+            }
+        }
+        return continentsEnBesoin;
     }
 }
