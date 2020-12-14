b800497d11f4b3777a00a9763a5cacd57718d7b2
lisa <doyenlisa@gmail.com>
2020-12-14 16:42:12 +0100

    ajout test

--- test/CO2/SommetUnitTest.java
+++ test/CO2/SommetUnitTest.java
@@ -1,15 +1,72 @@
 package CO2;
 
 import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SommetUnitTest {
 
     SommetTile sommetTile;
+    ArrayList<Subject> lst;
+
+    @Before
+    public void setup() {
+        lst = new ArrayList<Subject>();
+        for(int i=0; i<3;i++){
+            Subject sub = new Subject();
+            if(i == 0) sub.setEnergy(greenEnergyTypes.FUSION);
+            if(i == 1) sub.setEnergy(greenEnergyTypes.RECYCLING);
+            if(i == 2) sub.setEnergy(greenEnergyTypes.BIOMASS);
+            lst.add(sub);
+        }
+        Image imgSommet = new Image(getClass().getResourceAsStream("images/Sommets/Paris.png"));
+        ImageView imgSommetView = new ImageView(imgSommet);
+        Image imgContinent = new Image(getClass().getResourceAsStream("images/Sommets/Paris.png"));
+        Continent Europe = new Continent("Europe", 4, imgContinent,0);
+        sommetTile = new SommetTile("Paris", Europe, lst.size(), lst, imgSommetView);
+        sommetTile.setSubjects(lst);
+
+    }
+
+    @Test
+    public void testHaveEnergy() {
+        Assert.assertTrue(sommetTile.haveEnergy(greenEnergyTypes.FUSION));
+        Assert.assertTrue(sommetTile.haveEnergy(greenEnergyTypes.RECYCLING));
+        Assert.assertTrue(sommetTile.haveEnergy(greenEnergyTypes.BIOMASS));
+    }
+
+    @Test
+    public void testHaveEnergy2() {
+        Assert.assertFalse(sommetTile.haveEnergy(greenEnergyTypes.SOLAR));
+    }
+
+    @Test
+    public void testIsFull() {
+        Assert.assertFalse(sommetTile.isFull());
+    }
+
+    @Test
+    public void testIsFull2() {
+        Scientifique s = new Scientifique();
+        for (int i = 0; i<lst.size();i++) lst.get(i).setScientifique(s);
+        Assert.assertTrue(sommetTile.isFull());
+    }
+
+    @Test
+    public void testGetIndexSubject(){
+        Subject sub1 = new Subject();sub1.setEnergy(greenEnergyTypes.FUSION);
+        Subject sub2 = new Subject();sub2.setEnergy(greenEnergyTypes.RECYCLING);
+        Subject sub3 = new Subject();sub3.setEnergy(greenEnergyTypes.BIOMASS);
+        Assert.assertEquals(0, sommetTile.getIndexSubject(sub1));
+        Assert.assertEquals(1, sommetTile.getIndexSubject(sub2));
+        Assert.assertEquals(2, sommetTile.getIndexSubject(sub3));
+    }
+
 /*
     @Before
     public void setup() {
